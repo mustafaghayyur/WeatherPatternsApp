0e6feaf576976e1efdd83196b8136d113cf72571
Mustafa Ghayyur <mghayyur@gmail.com>
2018-01-20 21:51:25 -0500

    polished up the app. Also made it production ready (i.e. removed Npm dependency).

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,17 @@ import WeatherList from '../containers/weather_list';
 export default class App extends Component {
   render() {
     return (
-      	<div>
+      	<div className="app">
+      		<div className="app-header">
+	      		<h1>Waether Patterns App</h1>
+	      		<h5>
+	      			Search for any Canadian city below to get the weather patterns for the next 5 days!
+	      		</h5>
+      		</div>
       		<SearchBar />
+      		<h4 className="small temp-legend">
+      			Temperature in Kelvin ([K] = [°C] + 273.15)
+      		</h4>
       		<WeatherList />
     	</div>
     );
